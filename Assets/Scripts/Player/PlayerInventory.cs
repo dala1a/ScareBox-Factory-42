deb888d6df500eff7b80ca946179cb20e3e18cca
dala1a <0jeonyunseo1107@gmail.com>
2025-06-03 21:38:33 -0400

    Revert "ice"
    This reverts commit 2ac5c940acf02a7f618da6260bdf1a82e1e7303f.

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -4,35 +4,14 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public InventoryObject inventory;
-    [SerializeField] Animator leverAnimator;
-    private bool toggleLever = false; 
 
     public void pickUpItem()
     {
-        // Debug.Log("INTERACTED");
+        Debug.Log("INTERACTED");
         var hitInfo = PlayerInteract.hitInfo;
         InteractableItems interactable = hitInfo.collider.GetComponent<InteractableItems>();
         inventory.addItem(interactable.item, 1);
         Destroy(hitInfo.transform.gameObject);
-    }
-
-    public void useLever()
-    {
-        Debug.Log("LEVER BNEING USED");
-        toggleLever = !toggleLever;
-        playLeverAnimation(); 
-    }
-
-    private void playLeverAnimation()
-    {
-        if (toggleLever == true)
-        {
-            leverAnimator.Play("LeverClose", 0, 0.0f);
-        }
-        else
-        { 
-            leverAnimator.Play("LeverOpen", 0, 0.0f);
-        }
     }
 
     private void OnApplicationQuit()
